f82653463ad2c8006dbe62a381a001bb048c1505
dogui <parachuter1542@hotmail.com>
2024-04-17 18:59:53 +0200

    main update package

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import "app/app.dart";
 
-import 'package:adopta_amigo/firebase_options.dart';
-
+import 'package:adoptAmigo/firebase_options.dart';
 
 void main() async {
-
- WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.currentPlatform,
- );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
-
-
