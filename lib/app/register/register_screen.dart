6901493c8e25e49bb3a2c210bb8fa0b37810834e
dogui <parachuter1542@hotmail.com>
2024-04-21 18:01:51 +0200

    register act

--- lib/app/register/register_screen.dart
+++ lib/app/register/register_screen.dart
@@ -20,7 +20,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
@@ -39,7 +39,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 40),
-          height: MediaQuery.of(context).size.height - 50,
+          // height: MediaQuery.of(context).size.height - 50,
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -183,6 +183,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       ),
                     ],
                   )),
+              FadeInUp(
+                  duration: Duration(milliseconds: 1200),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/logo1.jpg'),
+                            fit: BoxFit.scaleDown)),
+                  ))
             ],
           ),
         ),
