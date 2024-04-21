f228c3e94cd4e94170cf851b09be0fe620951e4f
dogui <parachuter1542@hotmail.com>
2024-04-21 18:03:15 +0200

    assets profile

--- lib/app/profile/profile_screen.dart
+++ lib/app/profile/profile_screen.dart
@@ -30,7 +30,7 @@ class ProfileScreen extends StatelessWidget {
                       children: const [
                         CircleAvatar(
                           maxRadius: 65,
-                          backgroundImage: AssetImage("iprofile.jpg"),
+                          backgroundImage: AssetImage("assets/iprofile.jpg"),
                         ),
                       ],
                     ),
