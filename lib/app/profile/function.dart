87d25c249879c413a1e63235b154b38bfac13fac
dogui <parachuter1542@hotmail.com>
2024-04-21 17:01:23 +0200

    actualizacion funtion. rethrow

--- lib/app/profile/function.dart
+++ lib/app/profile/function.dart
@@ -3,91 +3,74 @@ import 'package:adoptAmigo/app/model/pet.dart';
 import 'package:adoptAmigo/app/model/collaborator.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
 Future<Customer> getInfoUser() async {
   final user = FirebaseAuth.instance.currentUser;
   if (user != null) {
-    final ref = FirebaseFirestore.instance.collection('usuarios');
-
-    final res = await ref.where('email', isEqualTo: user.email).get();
-    final data = Customer.fromMap(res.docs[0].data());
-    return data;
-  }
-  throw Exception('No se encontro el usuario');
-}
-
-Future<Pet> getInfoPet([String? uid]) async {
-  final user = FirebaseAuth.instance.currentUser;
-  dynamic data;
-  if (user != null) {
     try {
-      final ref = FirebaseFirestore.instance.collection('animales');
-      await ref.doc(uid).get().then(
-        (DocumentSnapshot doc) {
-          data = doc.data() as Map<String, dynamic>;
-        },
-        onError: (e) => print("Error getting document: $e"),
-      );
-
-      return Pet.fromMap(data);
+      final ref = FirebaseFirestore.instance.collection('usuarios');
+      final res = await ref.where('email', isEqualTo: user.email).get();
+      if (res.docs.isEmpty) throw Exception('No se encontró el usuario');
+      final data = Customer.fromMap(res.docs[0].data());
+      return data;
     } catch (e) {
-      throw Exception('No se encontro la mascota');
+      print("Error getting user: $e");
+      rethrow; // Lanza la excepción para ser manejada por quien llama la función.
     }
+  } else {
+    throw Exception('No se encontro el usuario');
   }
-
-  throw Exception('No se encontro la mascota');
 }
 
-Future<Collaborator> getInfoCollaborator([String? uid]) async {
-  final user = FirebaseAuth.instance.currentUser;
-  dynamic data;
-  if (user != null) {
-    final ref = FirebaseFirestore.instance.collection('protectoras');
-
-    await ref.doc(uid).get().then(
-      (DocumentSnapshot doc) {
-        data = doc.data() as Map<String, dynamic>;
-      },
-      onError: (e) => print("Error getting document: $e"),
-    );
-
-    return Collaborator.fromMap(data);
+Future<Pet> getInfoPet(String uid) async {
+  try {
+    final ref = FirebaseFirestore.instance.collection('animales');
+    final docSnapshot = await ref.doc(uid).get();
+    if (!docSnapshot.exists) throw Exception('No se encontró la mascota');
+    final data = docSnapshot.data() as Map<String, dynamic>;
+    return Pet.fromMap(data);
+  } catch (e) {
+    print("Error getting pet: $e");
+    rethrow; // Lanza la excepción para ser manejada por quien llama la función.
   }
-  throw Exception('No se encontro el usuario');
+}
+
+Future<Collaborator> getInfoCollaborator(String uid) async {
+  try {
+    final ref = FirebaseFirestore.instance.collection('protectoras');
+    final docSnapshot = await ref.doc(uid).get();
+    if (!docSnapshot.exists) throw Exception('No se encontró el colaborador');
+    final data = docSnapshot.data() as Map<String, dynamic>;
+    return Collaborator.fromMap(data);
+  } catch (e) {
+    print("Error getting collaborator: $e");
+    rethrow; // Lanza la excepción para ser manejada por quien llama la función.
+  }
 }
 
 Future<bool> setAdoption(String idAnimal) async {
-  var ret = false;
+  try {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) throw Exception('Usuario no autenticado');
 
-  final user = FirebaseAuth.instance.currentUser;
+    final ref = FirebaseFirestore.instance.collection('adopciones');
+    final data = {"id_animal": idAnimal, "id_usuario": user.uid};
+    final documentSnapshot = await ref.add(data);
 
-  final ref = FirebaseFirestore.instance.collection('adopciones');
+    if (documentSnapshot.id == "") return false;
 
-  final data = {"id_animal": idAnimal, "id_usuario": user!.uid};
-
-  final res = await ref.add(data).then((documentSnapshot) => {
-        if (documentSnapshot.id != "")
-          {updateAnimal(idAnimal), ret = true}
-        else
-          {ret = false}
-      });
-
-  return ret;
-
-  //dos partes se guardan aqui, se guarda en la bd de adopciones
-  //y luego se actualiza el valor en animal
+    await updateAnimal(idAnimal);
+    return true;
+  } catch (e) {
+    print("Error setting adoption: $e");
+    return false;
+  }
 }
 
-bool updateAnimal(String idAnimal) {
-  var ret = false;
-
+Future<void> updateAnimal(String idAnimal) async {
   final animales = FirebaseFirestore.instance.collection('animales');
-  final animalDoc = animales.doc(idAnimal).update({"isAdopted": true}).then(
-      (value) => {ret = true},
-      onError: (e) => print("Error updating document $e"));
-
-  return true;
+  await animales.doc(idAnimal).update({"isAdopted": true});
 }
 
 Future<Map<dynamic, Pet>> getUserPetList() async {
